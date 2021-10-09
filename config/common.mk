b9bde9a56949bccda1ead61dedb7323b9d212cb3
Danny Lin <danny@kdrag0n.dev>
2021-10-09 06:24:31 -0700

    vendor: Enable one-handed mode
    There's nothing device-specific about this feature, so enable it for
    everyone.
    
    Change-Id: I81e7a4595a9590f05a034c4b62263f45e0ea411f
    Signed-off-by: Kunmun <kunmun.devroms@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -44,6 +44,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/citric/prebuilt/common/etc/init.local.rc:$(TARGET_COPY_OUT_SYSTEM)/etc/init/init.citric.rc
 
+# Enable one-handed mode
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.support_one_handed_mode=true
+
 # Backup Tool
 PRODUCT_COPY_FILES += \
     vendor/citric/build/tools/backuptool.sh:install/bin/backuptool.sh \
