ee7fd68207620e9854c0ee46437e892e417c18cd
Omkar Chandorkar <gotenksIN@aospa.co>
2022-09-13 18:41:08 +0530

    overlays: Import icon packs from AOSPA sapphire
    Change-Id: I8bb5f4ccff49a039e7352e4522447fcbca5c970c

--- overlay/overlays.mk
+++ overlay/overlays.mk
@@ -20,4 +20,34 @@ PRODUCT_PACKAGES += \
     FontOppoSansOverlay \
     FontRobotoOverlay \
     FontUrbanistOverlay \
-    PixelLauncherIconsOverlay
+    PixelLauncherIconsOverlay \
+    IconPackCircularAndroidOverlay \
+    IconPackCircularLauncherOverlay \
+    IconPackCircularSettingsOverlay \
+    IconPackCircularSystemUIOverlay \
+    IconPackCircularThemePickerOverlay \
+    IconPackVictorAndroidOverlay \
+    IconPackVictorLauncherOverlay \
+    IconPackVictorSettingsOverlay \
+    IconPackVictorSystemUIOverlay \
+    IconPackVictorThemePickerOverlay \
+    IconPackSamAndroidOverlay \
+    IconPackSamLauncherOverlay \
+    IconPackSamSettingsOverlay \
+    IconPackSamSystemUIOverlay \
+    IconPackSamThemePickerOverlay \
+    IconPackKaiAndroidOverlay \
+    IconPackKaiLauncherOverlay \
+    IconPackKaiSettingsOverlay \
+    IconPackKaiSystemUIOverlay \
+    IconPackKaiThemePickerOverlay \
+    IconPackFilledAndroidOverlay \
+    IconPackFilledLauncherOverlay \
+    IconPackFilledSettingsOverlay \
+    IconPackFilledSystemUIOverlay \
+    IconPackFilledThemePickerOverlay \
+    IconPackRoundedAndroidOverlay \
+    IconPackRoundedLauncherOverlay \
+    IconPackRoundedSettingsOverlay \
+    IconPackRoundedSystemUIOverlay \
+    IconPackRoundedThemePickerOverlay
