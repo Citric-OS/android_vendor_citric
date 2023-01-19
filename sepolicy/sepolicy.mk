4b6b23f2b9ef2de31f0ad733ddf717c4754fd74e
Tobias Merkel <stylogey@gmail.com>
2023-01-19 23:08:29 +0100

    sepolicy: PRODUCT_* -> SYSTEM_EXT_*
    Change-Id: I1825b4013cb2d8918cd4bd5c63ff43adb10af9ce

--- sepolicy/sepolicy.mk
+++ sepolicy/sepolicy.mk
@@ -4,7 +4,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-PRODUCT_PRIVATE_SEPOLICY_DIRS += \
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
     vendor/atomx/sepolicy/private
 
 BOARD_VENDOR_SEPOLICY_DIRS += \
