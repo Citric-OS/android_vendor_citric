88706845fbfec84a14bb34ba9cd935a7ec8a2382
Omkar Chandorkar <gotenksIN@aospa.co>
2023-11-19 15:48:18 +0900

    target: Inherit microsoft vendor
    Change-Id: I9e52ddbddb3e311c7dc47557e9934e62f610cd7c

--- target/product/atomx-target.mk
+++ target/product/atomx-target.mk
@@ -136,6 +136,9 @@ else
 $(call inherit-product-if-exists, vendor/google/modules/build/mainline_modules_flatten_apex.mk)
 endif
 
+# Include Microsoft prebuilts makefile
+$(call inherit-product, vendor/atomx/prebuilt/microsoft/packages.mk)
+
 # Include Overlay makefile
 $(call inherit-product, vendor/atomx/overlay/overlays.mk)
 
