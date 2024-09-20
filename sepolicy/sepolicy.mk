f413089d1528204c4684313cfb88ec2eb9aa1a2d
Kunmun <kunmun@aospa.co>
2024-09-20 20:01:50 +0530

    sepolicy: Build public policies
    Change-Id: Id9ac4289e419e28ceeb1ee4dfcc43ccc70d90f42

--- sepolicy/sepolicy.mk
+++ sepolicy/sepolicy.mk
@@ -1,11 +1,14 @@
 #
-# Copyright (C) 2022 The AtomX Project
+# Copyright (C) 2024 The AtomX Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
     vendor/atomx/sepolicy/private
+
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
+    vendor/atomx/sepolicy/public
 
 BOARD_VENDOR_SEPOLICY_DIRS += \
     vendor/atomx/sepolicy/vendor
